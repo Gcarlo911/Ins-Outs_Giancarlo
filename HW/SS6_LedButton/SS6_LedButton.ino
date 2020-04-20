713f39df19da328f7ec18634d16a0132aabe3b04
Gcarlo911 <gcarlo911@gmail.com>
2020-04-20 00:51:37 -0400

    Create SS6_LedButton.ino
    hw done

---
+++ HW/SS6_LedButton/SS6_LedButton.ino
@@ -0,0 +1,37 @@
+void setup() {
+  pinMode(5, INPUT);
+  pinMode(9, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
+
+}
+
+void loop() {
+
+
+if (digitalRead(5) == HIGH) {
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(200);
+ 
+}
+else if(digitalRead(5) == LOW) {
+digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(20);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(20);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, LOW);
+  delay(20);
+  digitalWrite(9, HIGH);
+  digitalWrite(10, HIGH);
+  digitalWrite(11, HIGH);
+  delay(20);
+}
+}
