2deb6b7b64b0f5c8a44af698bdb963a473debc5d
Gcarlo911 <gcarlo911@gmail.com>
2020-04-20 01:06:20 -0400

    Update SS6_LedButton.ino

--- HW/SS6_LedButton/SS6_LedButton.ino
+++ HW/SS6_LedButton/SS6_LedButton.ino
@@ -1,5 +1,6 @@
 void setup() {
   pinMode(5, INPUT);
+  pinMode(8, OUTPUT);
   pinMode(9, OUTPUT);
   pinMode(10, OUTPUT);
   pinMode(11, OUTPUT);
@@ -10,6 +11,7 @@ void loop() {
 
 
 if (digitalRead(5) == HIGH) {
+  digitalWrite(8, LOW);
   digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
@@ -17,18 +19,27 @@ if (digitalRead(5) == HIGH) {
  
 }
 else if(digitalRead(5) == LOW) {
-digitalWrite(9, LOW);
+  digitalWrite(8, LOW);
+  digitalWrite(9, LOW);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
   delay(20);
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(11, LOW);
+  delay(20);
+  digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(10, LOW);
   digitalWrite(11, LOW);
   delay(20);
+  digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(11, LOW);
   delay(20);
+  digitalWrite(8, HIGH);
   digitalWrite(9, HIGH);
   digitalWrite(10, HIGH);
   digitalWrite(11, HIGH);
